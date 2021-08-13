4f3271ac47b3e345e0841b3b30b596b72ef0ea64
Aphievel <83160024+Aphievel@users.noreply.github.com>
2021-08-13 10:55:15 +0530

    Added scoring system
    Also cleaned up code

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ class Paddle:
     def __init__(self):
         self.paddle_rect = pygame.Rect(0,0,20,100)
         self.score = 0
+    
+    def movement(self):
+        pass
 
     def update(self):
         pygame.draw.rect(screen, "Black", self.paddle_rect)
@@ -39,16 +42,20 @@ class Ball:
         self.velocity_x = 6
         self.velocity_y = randint(0,3)
 
-    def update(self, paddle_rect, paddle_rect_2):
+    def update(self):
         pygame.draw.rect(screen, "Black", self.ball_rect)
         self.movement()
-        self.collision(paddle_rect, paddle_rect_2)
         pygame.draw.rect(screen, "White", self.ball_rect)
 
     def collision(self, paddle_rect, paddle_rect_2):
-        if self.ball_rect.colliderect(paddle_rect) or self.ball_rect.colliderect(paddle_rect_2):
-            self.velocity_x = -self.velocity_x
-            self.velocity_y = -self.velocity_y
+        if self.ball_rect.left < 30:
+            if self.ball_rect.colliderect(paddle_rect):
+                self.velocity_x = -self.velocity_x
+                self.velocity_y = -self.velocity_y
+        if self.ball_rect.right > 470:
+            if self.ball_rect.colliderect(paddle_rect_2):
+                self.velocity_x = -self.velocity_x
+                self.velocity_y = -self.velocity_y
         if self.ball_rect.top < 0 or self.ball_rect.bottom > 500:
             self.velocity_y = -self.velocity_y
 
@@ -56,11 +63,16 @@ class Ball:
         self.ball_rect.x -= self.velocity_x
         self.ball_rect.y -= self.velocity_y
 
-    def reset_and_tally_score(self):
-        if self.ball_rect.right < 0:
+    def score_and_reset(self):
+        if self.ball_rect.right <= -20:
             self.ball_rect.center = (400, 250)
             self.velocity_y = randint(0,3)
-            return 1
+            return [0,1]
+        if self.ball_rect.left >= 820:
+            self.ball_rect.center = (400, 250)
+            self.velocity_y = randint(0,3)
+            return [1,0]
+        else: return []
 
 pygame.init()
 screen = pygame.display.set_mode((800, 500))
@@ -76,16 +88,34 @@ ball = Ball()
 while True:
     player_paddle.update()
     computer_paddle.update()
-    ball.update(player_paddle.paddle_rect, computer_paddle.paddle_rect)
+    ball.collision(player_paddle.paddle_rect, computer_paddle.paddle_rect)
+    ball.update()
+    return_list = ball.score_and_reset()
+
+    if return_list:
+        score_list = [player_paddle.score, computer_paddle.score]
+        for i in range(0,2):
+            score_list[i] =score_list[i] + return_list[i]
+        player_paddle.score = score_list[0]
+        computer_paddle.score = score_list[1]    
+
+    #fps
+    fps_surf = score_font.render(f"FPS:{int(fpsclock.get_fps())}",True, "White","Black")
+    fps_rect = fps_surf.get_rect(topright = (800, 0))
+    screen.blit(fps_surf, fps_rect)
 
     #score
-    score_surf = score_font.render(f"FPS:{int(fpsclock.get_fps())}",True, "White","Black")
-    score_rect = score_surf.get_rect(topright = (800, 0))
-    screen.blit(score_surf, score_rect)
+    player_score_surf = score_font.render(f"{player_paddle.score}",True, "White", "Black")
+    player_score_rect = player_score_surf.get_rect(center = (200, 125))
+    screen.blit(player_score_surf, player_score_rect)
+
+    computer_score_surf = score_font.render(f"{computer_paddle.score}",True, "White", "Black")
+    computer_score_rect = computer_score_surf.get_rect(center = (600, 125))
+    screen.blit(computer_score_surf, computer_score_rect)
 
     pygame.draw.aaline(screen, "White", (400,0), (400,500))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys_exit()
     pygame.display.update()
-    fpsclock.tick(60)
+    fpsclock.tick(FPS)
